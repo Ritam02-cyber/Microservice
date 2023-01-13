b76d11e449b1ac44dadc85453c655f9e95d58708
Ritam02-cyber <nathritam02@gmail.com>
2023-01-13 12:35:27 +0530

    Initial commit

---
+++ ErrorHandler/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const errorHandler = require('./middleware/errorhandler')
+const routes = require('./routes/routes')
+app.use(express.json())
+
+app.listen(3000, ()=>{
+    console.log("Server started");
+    
+})
+
+app.use(errorHandler)
+app.use('/api', routes)
